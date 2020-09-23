f3f7f7efacbd8cd7882fc6d94d511638052ab122
artxty <websheud@gmail.com>
2020-09-23 09:31:54 -0700

    chore(helpers): remove unnecessary

--- src/helpers.js
+++ src/helpers.js
@@ -1,13 +1,7 @@
-const capitalize = (string = '') => {
-  const [firstLetter, ...rest] = string.split('');
-  return [firstLetter.toUpperCase(), ...rest].join('');
-};
-
 const getFullKey = (storageName, key) => `@${storageName}-${key}`;
 const getShortKey = (storageName, fullKey) => fullKey.split(`@${storageName}-`)[1];
 
 module.exports = {
-  capitalize,
   getFullKey,
   getShortKey,
 };
